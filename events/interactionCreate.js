2ff6cfc37ff60d3f40a98f37cc39d960b1d53935
Gifaldy Azka <68645946+gifaldyazkaa@users.noreply.github.com>
2021-10-27 15:53:56 +0700

    chore(v1): send error to debug channel (#5)

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,11 @@
 const client = require('../index');
+const { MessageEmbed } = require('discord.js');
 
 client.on('interactionCreate', async (interaction) => {
+  // Debug channel declarations
+  const debugChannel = client.channels.cache.find(
+    (channel) => channel.id === '899997057849393225'
+  );
   // Slash Command Handling
   if (interaction.isCommand()) {
     await interaction.deferReply({ ephemeral: false }).catch(() => {});
@@ -25,6 +30,12 @@ client.on('interactionCreate', async (interaction) => {
     try {
       cmd.run(client, interaction, args);
     } catch (err) {
+      const embed = new MessageEmbed()
+        .setTitle(':x: An Error Occured')
+        .setDescription(`\`\`\`yml\n${err}\n\`\`\``)
+        .setColor('RED')
+        .setTimestamp();
+      debugChannel.send({ embeds: [embed] });
       console.log(err);
     }
   }
