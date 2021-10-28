837f6d9b082bcc155f664dc1c33ee7125f8287fc
Yuki Shira <91227737+shiraayuki@users.noreply.github.com>
2021-10-28 11:31:26 +0700

    chore(v1): update presence

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,5 @@ const client = require('../index');
 client.on('ready', () => {
   console.log(`Bot is ready!`);
   client.user.setStatus('idle');
-  client.user.setActivity({ name: 'Falxxdev <3', type: 'LISTENING' });
+  client.user.setActivity({ name: `${message.client.users.cache.size} Members`, type: 'LISTENING' });
 });
