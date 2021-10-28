41ae1a1bc69f590d8ff3e796995f8862e7e84e60
Yuki Shira <91227737+shiraayuki@users.noreply.github.com>
2021-10-28 12:12:25 +0700

    feat(v1): add uptime command (#7)
    * feat(v1): add uptime command
    
    * chore(v1): prettify ms with pretty-ms
    
    * feat(v1): add uptime slash command

---
+++ commands/Info/uptime.js
@@ -0,0 +1,19 @@
+const { Message, Client, MessageEmbed } = require('discord.js');
+const pretty = require('pretty-ms');
+
+module.exports = {
+  name: 'uptime',
+  aliases: ['up'],
+  /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    const embed = new MessageEmbed()
+      .setDescription(`🕘 Uptime : ${pretty(client.uptime)}ms!`)
+      .setColor('FUCHSIA');
+    message.channel.send({ embeds: [embed] });
+  },
+};
