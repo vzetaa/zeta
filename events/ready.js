dbfb86ad95b0af471fe655a8d20f29feb7d713bc
Yuki Shira <91227737+shiraayuki@users.noreply.github.com>
2021-10-28 11:44:34 +0700

    chore(v1): format code

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,8 @@ const client = require('../index');
 client.on('ready', () => {
   console.log(`Bot is ready!`);
   client.user.setStatus('idle');
-  client.user.setActivity({ name: `${client.users.cache.size} Members`, type: 'LISTENING' });
+  client.user.setActivity({
+    name: `${client.users.cache.size} Members`,
+    type: 'LISTENING',
+  });
 });
