4e42d96ecc7b5336e36ef7f1658e632ff34426ba
gifaldyazkaa <official.gifaldy@gmail.com>
2021-12-03 22:45:09 +0700

    chore: update help commands

--- src/Commands/Info/Help.ts
+++ src/Commands/Info/Help.ts
@@ -11,8 +11,9 @@ export const command: Command = {
 	run: async (client, message, args) => {
 		const embed = new MessageEmbed()
 			.setTitle('Kogasa Help Desk')
+			.setImage(`https://cdn.upload.systems/uploads/jIwkxukV.png`)
 			.setDescription(
-				`Hello there! I'm Kogasa here~! Maids and Helpers in this server!\n\nLooking for Command List? Oh, Here is it!\n\nYou can see Full Command List at\n[gifaldyazka.is-a.dev/kogasa-dscbot](https://gifaldyazka.is-a.dev/kogasa-dscbot)!`
+				`Hello there! I'm Kogasa here~! Maids and Helpers in this server! ☂️\n\nLooking for Command List? Oh, Here is it!\n\nYou can see Full Command List at\n[gifaldyazka.is-a.dev/kogasa-dscbot/p/commands](https://gifaldyazka.is-a.dev/kogasa-dscbot/p/commands)!`
 			)
 			.setThumbnail(message.client.user.displayAvatarURL({ size: 512 }))
 			.setColor('PURPLE')
--- src/Slash/Info/Help.ts
+++ src/Slash/Info/Help.ts
@@ -9,8 +9,9 @@ export const slash: Slash = {
 	run: async (client, interaction, args) => {
 		const embed = new MessageEmbed()
 			.setTitle('Kogasa Help Desk')
+			.setImage(`https://cdn.upload.systems/uploads/jIwkxukV.png`)
 			.setDescription(
-				`Hello there! I'm Kogasa here~! Maids and Helpers in this server!\n\nLooking for Command List? Oh, Here is it!\n\nYou can see Full Command List at\n[gifaldyazka.is-a.dev/kogasa-dscbot](https://gifaldyazka.is-a.dev/kogasa-dscbot)!`
+				`Hello there! I'm Kogasa here~! Maids and Helpers in this server! ☂️\n\nLooking for Command List? Oh, Here is it!\n\nYou can see Full Command List at\n[gifaldyazka.is-a.dev/kogasa-dscbot/p/commands](https://gifaldyazka.is-a.dev/kogasa-dscbot/p/commands)!`
 			)
 			.setThumbnail(interaction.client.user.displayAvatarURL({ size: 512 }))
 			.setColor('PURPLE')
