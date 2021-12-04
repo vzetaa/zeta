19cdf932886769dc9118be9c5494326f429098ce
gifaldyazkaa <official.gifaldy@gmail.com>
2021-12-04 23:42:23 +0700

    feat: implement kill command

---
+++ src/Commands/Admin/Kill.ts
@@ -0,0 +1,38 @@
+import { MessageEmbed } from 'discord.js';
+import { Command } from '../../interfaces';
+
+export const command: Command = {
+	name: 'kill',
+	description: 'Kill client sessions',
+	aliases: ['destroy'],
+	usage: '[reasons]',
+	testOnly: false,
+	permissions: ['SEND_MESSAGES'],
+	run: async (client, message, args) => {
+		// Reasons declaration
+		let reasons = args.join(' ');
+
+		// Owner ID Checking & Reasons Checking
+		if (message.author.id !== process.env.OWNERID) return;
+		if (!reasons) reasons = 'No reasons.';
+
+		// Send success message
+		await message.channel.send({
+			embeds: [
+				new MessageEmbed()
+					.setTitle('✅ Successfully Killed!')
+					.setThumbnail(client.user.displayAvatarURL({ size: 512 }))
+					.setDescription(
+						`Successfully Destroyed Client Sessions with Exit Code 1`
+					)
+					.addField('Reasons', `\`${reasons}\``)
+					.setColor('GREEN')
+					.setFooter(`Restarted by ${message.author.username}`)
+					.setTimestamp(),
+			],
+		});
+
+		// Destroy the client
+		return client.destroy();
+	},
+};
