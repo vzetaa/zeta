1e91652966e9c9d7551173a021c876651ccdc220
gifaldyazkaa <official.gifaldy@gmail.com>
2021-12-14 22:14:49 +0700

    fix: yarnx is not found

--- src/Bin/Build.ts
+++ src/Bin/Build.ts
@@ -2,7 +2,7 @@ import { exec } from 'child_process';
 import consola from 'consola';
 
 consola.info('Building source file from ./src...');
-exec('yarnx run build:native', (err, stdout, stderr) => {
+exec('yarn run build:native', (err, stdout, stderr) => {
 	if (stderr) return consola.error(stderr);
 	consola.success('Successfully transpiled source file into ./dist');
 	consola.log('🚀 Source file has been transpiled to JavaScript code!');
