9ed1d8c0e9bbc8b3d5aeb1f78b0d035c8ab1d6f6
gifaldyazkaa <official.gifaldy@gmail.com>
2021-12-15 02:34:06 +0700

    refactor: refactor terminal.ts

--- src/Commands/Admin/Terminal.ts
+++ src/Commands/Admin/Terminal.ts
@@ -1,6 +1,6 @@
-import child from 'child_process';
 import { MessageEmbed } from 'discord.js';
 import { Command } from '../../Interfaces';
+import { exec } from 'child_process';
 
 export const command: Command = {
 	name: 'terminal',
@@ -14,25 +14,22 @@ export const command: Command = {
 		if (message.author.id !== process.env.OWNERID) return;
 		if (!cmds) return message.reply('Please specify a command to executed!');
 
-		try {
-			child.exec(cmds, (err, res) => {
-				if (err)
-					return message.channel.send({
-						content: `zsh: cocmmad not found ${cmds}`,
-					});
-				message.channel.send({
-					embeds: [
-						new MessageEmbed()
-							.setTitle('Terminal - GNU Bash')
-							.setDescription(`\`\`\`js\n${res.slice(0, 2000)}\`\`\``)
-							.setFooter(`GNU Bash - Actioned by ${message.author.tag}`)
-							.setColor('LUMINOUS_VIVID_PINK'),
-					],
+		exec(cmds, (err, stdout, stderr) => {
+			if (err)
+				return message.channel.send({ content: `\`\`\`\n${err}\n\`\`\`` });
+			if (stderr)
+				return message.channel.send({
+					content: `zsh: command not found: ${cmds}`,
 				});
+			message.channel.send({
+				embeds: [
+					new MessageEmbed()
+						.setTitle('Terminal - GNU Bash')
+						.setDescription(`\`\`\`js\n${stdout.slice(0, 2000)}\`\`\``)
+						.setFooter(`GNU Bash - Actioned by ${message.author.tag}`)
+						.setColor('LUMINOUS_VIVID_PINK'),
+				],
 			});
-		} catch (err) {
-			message.channel.send({ content: `zsh: cocmmad not found ${cmds}` });
-			console.log(err);
-		}
+		});
 	},
 };
