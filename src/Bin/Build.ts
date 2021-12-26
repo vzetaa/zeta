251ba2d542b833479d41ce735657f9252942bd85
Josh <91227737+jstcl@users.noreply.github.com>
2021-12-26 11:47:39 +0700

    chore: remove build binary

--- src/Bin/Build.ts
+++
@@ -1,9 +0,0 @@
-import { exec } from 'child_process';
-import consola from 'consola';
-
-consola.info('Building source file from ./src...');
-exec('yarn run build:native', (err, stdout, stderr) => {
-	if (stderr) return consola.error(stderr);
-	consola.success('Successfully transpiled source file into ./dist');
-	consola.log('🚀 Source file has been transpiled to JavaScript code!');
-});
