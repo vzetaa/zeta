930a07d99d502fe206ad9a8f93c75e6aff4d4745
gifaldyazkaa <official.gifaldy@gmail.com>
2022-01-24 15:52:46 +0700

    feat: export azurlane fetch functions

---
+++ src/Exports/AzurLane.ts
@@ -0,0 +1,57 @@
+import fetch from 'node-fetch';
+
+// Search & List Ships
+const searchShip = async (name: string) => {
+	const req = await fetch(
+		`https://formidable.kashima.moe/ships/search?name=${encodeURI(name)}`
+	);
+	const body = await req.json();
+
+	return body[0];
+};
+
+const searchShipById = async (id: string) => {
+	const req = await fetch(
+		`https://formidable.kashima.moe/ships/id?code=${encodeURI(id)}`
+	);
+	const body = await req.json();
+
+	return body;
+};
+
+const listRandomShip = async () => {
+	const req = await fetch(`https://formidable.kashima.moe/ships/random`);
+	const body = await req.json();
+
+	return body;
+};
+
+// Search Equipments
+
+const searchEquipments = async (name: string) => {
+	const req = await fetch(
+		`https://formidable.kashima.moe/equipments/search?name=${encodeURI(name)}`
+	);
+	const body = await req.json();
+
+	return body[0];
+};
+
+// Search Voice Lines
+
+const searchVoiceLines = async (id: string) => {
+	const req = await fetch(
+		`https://formidable.kashima.moe/voicelines/ship?id=${encodeURI(id)}`
+	);
+	const body = await req.json();
+
+	return body;
+};
+
+export default {
+	searchShip,
+	searchShipById,
+	listRandomShip,
+	searchEquipments,
+	searchVoiceLines,
+};
