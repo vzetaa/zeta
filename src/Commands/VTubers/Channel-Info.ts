5525045cbc9f20f570d1c422cd029d98fbda30b9
Gifaldy Azka <falcxxdev@pm.me>
2022-03-03 16:18:22 +0700

    feat: use native color instead of using color-thief

--- src/Commands/VTubers/Channel-Info.ts
+++ src/Commands/VTubers/Channel-Info.ts
@@ -1,7 +1,6 @@
-import { getColorFromURL } from 'color-thief-node';
+import { ColorResolvable, MessageEmbed } from 'discord.js';
 import holoClient from '../../Exports/Holodex';
 import { Command } from '../../Interfaces';
-import { MessageEmbed } from 'discord.js';
 const numberWithCommas = (int) => {
 	// ong, regex :/
 	return int.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
@@ -16,35 +15,33 @@ export const command: Command = {
 	permissions: ['SEND_MESSAGES'],
 	run: async (client, message, args) => {
 		const channelId: string = args.join(' ');
+		const color: ColorResolvable = message.guild.me.displayHexColor;
 		if (!channelId)
 			return message.reply({ content: 'Please specify channel id!' });
 		holoClient
 			.getChannel(`${channelId}`)
 			.then((ch) => {
-				(async () => {
-					const accentColor = await getColorFromURL(ch.bannerUrl);
-					const embed = new MessageEmbed()
-						.setTitle(`${ch.name}`)
-						.setThumbnail(`${ch.avatarUrl}`)
-						.setImage(`${ch.bannerUrl}`)
-						.setColor(accentColor)
-						.setDescription(
-							`Channel Name : ${ch.name}\nEnglish Name : ${
-								ch.englishName
-							}\nAffilation : ${
-								ch.organization
-							}\nSubscribers : ${numberWithCommas(
-								ch.subscriberCount
-							)}\nTotal videos : ${
-								ch.videoCount
-							} videos\nTotal views : ${numberWithCommas(
-								ch.viewCount
-							)}\nTwitter : [@${ch.twitterName}](https://twitter.com/${
-								ch.twitterName
-							})`
-						);
-					message.channel.send({ embeds: [embed] });
-				})();
+				const embed = new MessageEmbed()
+					.setTitle(`${ch.name}`)
+					.setThumbnail(`${ch.avatarUrl}`)
+					.setImage(`${ch.bannerUrl}`)
+					.setColor(color)
+					.setDescription(
+						`Channel Name : ${ch.name}\nEnglish Name : ${
+							ch.englishName
+						}\nAffilation : ${
+							ch.organization
+						}\nSubscribers : ${numberWithCommas(
+							ch.subscriberCount
+						)}\nTotal videos : ${
+							ch.videoCount
+						} videos\nTotal views : ${numberWithCommas(
+							ch.viewCount
+						)}\nTwitter : [@${ch.twitterName}](https://twitter.com/${
+							ch.twitterName
+						})`
+					);
+				message.channel.send({ embeds: [embed] });
 			})
 			.catch((err) => console.log(err));
 	},
--- src/Commands/VTubers/Video-metadata.ts
+++ src/Commands/VTubers/Video-metadata.ts
@@ -1,8 +1,10 @@
-import { getColorFromURL } from 'color-thief-node';
-import { MessageEmbed } from 'discord.js';
+import { ColorResolvable, MessageEmbed } from 'discord.js';
 import { Command } from '../../Interfaces';
 import holoClient from '../../Exports/Holodex';
 import moment from 'moment';
+const capitalize = (str) => {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+};
 
 export const command: Command = {
 	name: 'videoinfo',
@@ -13,28 +15,25 @@ export const command: Command = {
 	permissions: ['SEND_MESSAGES'],
 	run: async (client, message, args) => {
 		const videoId = args.join(' ');
+		const color: ColorResolvable = message.guild.me.displayHexColor;
 		if (!videoId) return message.reply({ content: 'Please specify video id!' });
 		holoClient
-			.getVideo('kKTb4lpGFys')
+			.getVideo(`${videoId}`)
 			.then((vid) => {
 				const youtubeLink = `https://youtu.be/${vid.videoId}`;
-				(async () => {
-					const accentColor = await getColorFromURL(vid.channel.bannerUrl);
-					const embed = new MessageEmbed()
-						.setTitle(`${vid.title}`)
-						.setURL(`${youtubeLink}`)
-						.setThumbnail(`${vid.channel.avatarUrl}`)
-						.setColor(accentColor)
-						.setDescription(
-							`Type : ${vid.videoType.toUpperCase()}\nTopic : ${vid.topic.toUpperCase()}\nStart / Uploaded at : ${moment(
-								vid.actualStart
-							).format('llll')}\nEnded at : ${moment(vid.actualEnd).format(
-								'llll'
-							)}`
-						);
-					message.channel.send({ embeds: [embed] });
-					console.log(embed);
-				})();
+				const embed = new MessageEmbed()
+					.setTitle(`${vid.title}`)
+					.setURL(`${youtubeLink}`)
+					.setThumbnail(`${vid.channel.avatarUrl}`)
+					.setColor(color)
+					.setDescription(
+						`Type : ${capitalize(vid.videoType)}\nTopic : ${capitalize(
+							vid.topic
+						)}\nStart / Uploaded at : ${moment(vid.actualStart).format(
+							'llll'
+						)}\nEnded at : ${moment(vid.actualEnd).format('llll')}`
+					);
+				message.channel.send({ embeds: [embed] });
 			})
 			.catch((err) => console.log(err));
 	},
