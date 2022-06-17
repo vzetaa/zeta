414d1de82e68dee06eb19d3376a261cc74bf2384
Gifaldy Azka <falcxxdev@pm.me>
2022-06-17 18:16:43 +0000

    feat: use dynamic color

--- src/Commands/Utility/ClearMessage.ts
+++ src/Commands/Utility/ClearMessage.ts
@@ -1,4 +1,4 @@
-import { MessageEmbed } from 'discord.js';
+import { ColorResolvable, MessageEmbed } from 'discord.js';
 import { Command } from '../../Interfaces';
 
 export const command: Command = {
@@ -10,6 +10,7 @@ export const command: Command = {
     permissions: ['SEND_MESSAGES', 'MANAGE_MESSAGES'],
     run: async (client, message, args) => {
         const amount = Number(args[0]) || parseInt(args[0]);
+        const color: ColorResolvable = message.guild.me.displayHexColor;
         if (isNaN(amount) || !Number.isInteger(amount))
             return message.reply({
                 content: `Please enter a number of message to clear!`,
@@ -23,7 +24,7 @@ export const command: Command = {
             if (message.channel.type === 'GUILD_TEXT')
                 await message.channel.bulkDelete(amount).then(async m => {
                     let embed = new MessageEmbed()
-                        .setColor('#00ffff')
+                        .setColor(color)
                         .setDescription(`:white_check_mark:  Cleared **${m.size}**/**${amount}** messages!`);
 
                     message.channel.send({ embeds: [embed] }).then(msg => setTimeout(() => msg.delete(), 4000));
