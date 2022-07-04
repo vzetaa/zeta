97aabce57c68f2f24a4e114d22c8ee2abe1e9f38
Gifaldy Azka <me@falcxxdev.ml>
2022-07-04 08:40:55 +0700

    feat: define interface

--- src/Exports/LewImg.ts
+++ src/Exports/LewImg.ts
@@ -2,11 +2,10 @@ import fetch, { Response } from 'node-fetch';
 
 // Nekos.life
 /**
- *
  * @param type
  * @description Returns a data from nekos.life
  */
-const nekoslife = async (type: string) => {
+const nekoslife: (type: string) => Promise<any> = async (type: string) => {
     if (!type) return 'Please specify the type / endpoint!';
     const req: Response = await fetch(`https://nekos.life/api/v2/img/${type}`);
     const res = await req.json();
@@ -15,11 +14,10 @@ const nekoslife = async (type: string) => {
 
 // Waifu.pics
 /**
- *
  * @param type
  * @description Returns a data from waifu.pics
  */
-const waifupics = async (type: string) => {
+const waifupics: (type: string) => Promise<any> = async (type: string) => {
     const req: Response = await fetch(`https://api.waifu.pics/nsfw/${type}`);
     const res = await req.json();
     return res;
@@ -27,11 +25,10 @@ const waifupics = async (type: string) => {
 
 // Nekobot.xyz
 /**
- *
  * @param type
  * @description Returns a data from nekobot.xyz
  */
-const nekobot = async (type: string) => {
+const nekobot: (type: string) => Promise<any> = async (type: string) => {
     const req: Response = await fetch(`https://nekobot.xyz/api/image?type=${type}`);
     const res = await req.json();
     return res;
